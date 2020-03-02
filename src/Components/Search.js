3d9c43b918e213a721a5ee932536980e70aa6325
ArkadiiRaih <ArkadioRaih@yandex.ru>
2020-03-03 02:35:21 +1000

    Changed stylesheets

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,30 +1,62 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+
+import { Translate, I18n } from "react-redux-i18n";
+
 import { changeFilter } from "../actions/filterActions";
 import { findName } from "../utilities";
 
-function Search({ searchTerm, changeFilter, people }) {
+function Search({ locale, changeFilter, people }) {
   const [term, setTerm] = useState("");
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (!term) return;
+    // if (!term) return;
 
     if (changeFilter) changeFilter({ searchTerm: term });
     setTerm("");
   };
 
+  const handleClick = e => {
+    const { name } = e.target.dataset;
+    console.log(e.target);
+
+    if (changeFilter) changeFilter({ searchTerm: name });
+    setTerm("");
+  };
+
+  // const handleBlur = e => {
+  //   console.log("bluuuur");
+  //   console.log(e.target);
+  //   if (e.target.matches(".suggestions")) return;
+
+  //   setShowSuggestions(false);
+  // };
+
   return (
-    <form className="search-form" onSubmit={handleSubmit}>
+    <form
+      className="search-form"
+      onSubmit={handleSubmit}
+      onFocus={() => setShowSuggestions(true)}
+      onBlur={() => setTimeout(() => setShowSuggestions(false), 200)}
+    >
       <input
         type="text"
-        placeholder="search"
+        placeholder={I18n.t("search.find", { locale })}
         value={term}
         onChange={e => setTerm(e.target.value)}
       />
-      <button type="submit">find</button>
+      <button type="submit">
+        <Translate value="search.search" />
+      </button>
       {term === "" ? null : (
-        <ul className="suggestions">
+        <ul
+          className={`suggestions ${
+            showSuggestions ? "" : "suggestions_hidden"
+          }`}
+          onClick={handleClick}
+        >
           {findName(people, term)
             .slice(0, 10)
             .map((name, idx) => {
@@ -33,6 +65,7 @@ function Search({ searchTerm, changeFilter, people }) {
               return (
                 <li
                   key={`${name}-${idx}`}
+                  data-name={name}
                   dangerouslySetInnerHTML={{
                     __html: name.replace(
                       regex,
@@ -48,9 +81,10 @@ function Search({ searchTerm, changeFilter, people }) {
   );
 }
 
-const mapStateToProps = ({ filter, people }) => ({
+const mapStateToProps = ({ filter, people, i18n }) => ({
   searchTerm: filter.searchTerm,
-  people: people.people
+  people: people.people,
+  locale: i18n.locale
 });
 const mapDispatchToProps = {
   changeFilter
